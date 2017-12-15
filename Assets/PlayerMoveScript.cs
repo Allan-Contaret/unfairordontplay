036fadddf821737ce7b17f24350bf227c5f002f9
Ivan <ivan.peric93100@gmail.com>
2017-12-15 09:56:47 +0100

    personnage principal, déplacements et animation

---
+++ Assets/PlayerMoveScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+    public float SpeedMovement;
+    public float JumpPower;
+    public bool CanJump = false;
+    public float MaxSpeed;
+    private bool isArrowButtonClicked = false;
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey("space")  || Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right"))
+        {
+            isArrowButtonClicked = true;
+            animator.SetBool("IsArrowButtonClicked", isArrowButtonClicked);
+        } else
+        {
+            isArrowButtonClicked = false;
+            animator.SetBool("IsArrowButtonClicked", isArrowButtonClicked);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (GetComponent<Rigidbody>().velocity.magnitude < MaxSpeed)
+        {
+            if (Input.GetKey("up"))
+            {
+                GetComponent<Rigidbody>().AddForce(transform.forward * SpeedMovement);
+            }
+            if (Input.GetKey("down"))
+            {
+                GetComponent<Rigidbody>().AddForce(-transform.forward * SpeedMovement);
+            }
+            if (Input.GetKey("left"))
+            {
+                GetComponent<Rigidbody>().AddForce(-transform.right * SpeedMovement);
+            }
+            if (Input.GetKey("right"))
+            {
+                GetComponent<Rigidbody>().AddForce(transform.right * SpeedMovement);
+            }
+        } 
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, JumpPower, 0));
+        }
+        CanJump = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        CanJump = true;
+    }
+}
