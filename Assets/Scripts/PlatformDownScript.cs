ea2ea24b9d210deea4f2590eb44f1bf519f6b556
allancontaret <allan.contaret@ynov.com>
2017-12-18 17:41:06 +0100

    prefab platformdown

---
+++ Assets/Scripts/PlatformDownScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDownScript : MonoBehaviour {
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player") {
+			GetComponent<Rigidbody> ().useGravity = true;
+			GetComponent<Rigidbody> ().mass = 5f;
+		}
+	}
+}
