aa5b38bdfb2a5fc2ac75f9aeefad9d4739455b88
Allan-Contaret <allan.contaret@ynov.com>
2017-12-18 19:09:58 +0100

    Prefabs du caillou et de l'invisibleBlock et script du lancer

---
+++ Assets/Scripts/EjectScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjectScript : MonoBehaviour
+{
+
+    private bool CanTake = false;
+    private GameObject Eject;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F) && CanTake && Eject != null)
+        {
+            //Eject.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            Eject.transform.position = GameObject.Find("EjectPosition").transform.position;
+            Eject.gameObject.transform.parent = GameObject.Find("EjectPosition").transform;
+        }
+
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.tag == "Rock")
+        {
+            Eject = collider.gameObject;
+            CanTake = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+
+        if (collider.gameObject.tag == "")
+        {
+            Eject = null;
+            CanTake = false;
+        }
+    }
+}
