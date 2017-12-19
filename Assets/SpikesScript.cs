770233bb6a4b598fbd02429854ba449d4e79d4ce
Ivan <ivan.peric93100@gmail.com>
2017-12-19 21:36:41 +0100

    piège piques sortant du sol (prefab et script)

---
+++ Assets/SpikesScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesScript : MonoBehaviour {
+    public bool IsOpen = false;
+    private Vector3 StartPosition;
+    private Vector3 EndPosition;
+
+    //public Color StColor, EndColor, CurrentColor;
+
+
+    private float timer = 0;
+
+    void Start()
+    {
+        StartPosition = transform.localPosition;
+        EndPosition = StartPosition + new Vector3(0, 2f, 0);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            IsOpen = true;
+            timer = 0;
+            Destroy(other.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOpen)
+            timer += 4*Time.deltaTime;
+        else
+            timer -= Time.deltaTime;
+
+        transform.localPosition = Vector3.Lerp(StartPosition, EndPosition, timer);
+    }
+}
