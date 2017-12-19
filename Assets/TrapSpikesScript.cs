b8b72f3f8d621ed16198bb4bb8398999c6d700e0
Ivan <ivan.peric93100@gmail.com>
2017-12-19 19:32:37 +0100

    asset pièges, prefab du piège ejectable avec script associé

---
+++ Assets/TrapSpikesScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSpikesScript : MonoBehaviour
+{
+    public GameObject Player;
+    public Transform PlayersPosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            Player.GetComponent<Rigidbody>().AddForce(-PlayersPosition.forward * 2000);
+    }
+}
