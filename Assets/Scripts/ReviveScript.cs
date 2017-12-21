22818c78e730ff61540acf8ffbdf53fc8594d15f
allancontaret <allan.contaret@ynov.com>
2017-12-21 16:34:21 +0100

    modification du prefab du perso

---
+++ Assets/Scripts/ReviveScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReviveScript : MonoBehaviour {
+    public GameObject PrefabPlayer;
+
+	// Update is called once per frame
+	void Update () {
+		if(!GameObject.FindGameObjectWithTag("Player"))
+        {
+            GameObject Player = Instantiate<GameObject>(PrefabPlayer);
+        }
+	}
+}
