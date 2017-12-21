23f89a233fe5a76104afb65457f6f0a0e9aa2494
Allan-Contaret <allan.contaret@ynov.com>
2017-12-21 23:44:33 +0100

    prefab et script tourelles

---
+++ Assets/Scripts/TowerShootScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShootScript : MonoBehaviour
+{
+
+    public GameObject PrefabBullet;
+    public Transform BulletStartPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator Shoot()
+    {
+
+        while (GameObject.Find("SJ001_run"))
+        {
+            GameObject Bullet = Instantiate<GameObject>(PrefabBullet);
+            Bullet.transform.position = BulletStartPosition.position;
+            Bullet.GetComponent<Rigidbody>().AddForce(BulletStartPosition.forward * 2000);
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
